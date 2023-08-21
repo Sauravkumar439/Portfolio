b055b09b74a3d8936515e1b11b35c4e608ca755c
Your Name <you@example.com>
2023-08-22 00:11:32 +0530

    commit

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+                </div>
+                <p className='text-xl mt-20'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sint aspernatur distinctio quaerat ullam nostrum. Doloremque molestias, facere perspiciatis cum nihil animi dolores architecto quibusdam, sequi, odio amet fuga optio culpa. Modi ipsam voluptates dolorum nobis corrupti molestiae autem eaque, labore recusandae. Ipsa quasi, necessitatibus impedit dicta quos officia consequuntur!
+
+                </p>
+                <br/>
+                <p className='text-xl'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque at cupiditate a quaerat adipisci dignissimos ad laborum, minima tempora commodi nemo libero magnam quidem vel aperiam ullam sint ratione voluptatum!
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
